135f149a8731e5b85dd401775cc0dc8e04794072
HA-ANgit <hannes93andersson@gmail.com>
2022-02-24 13:31:36 +0100

    Update on startingpage

--- src/App.js
+++ src/App.js
@@ -2,11 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let audio = new Audio('/Metronome.mp3')
+
+  const metronome = () => {
+    audio.play()
+  }
+
   return (
+
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1 className="app-title">I am Drum Machine [┐∵]┘</h1>
+
+        < div id="Metronome" >
+      <button onClick={metronome}>Play</button>
+        </div >
+
         <p>
           En tempo-baserad trummaskin
         </p>
